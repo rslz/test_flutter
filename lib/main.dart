282a5eadda03635e33b1cc64b0de323b519bf230
rslz <rslzsy@gmail.com>
2022-03-09 12:29:08 +0800

    no message

--- lib/main.dart
+++ lib/main.dart
@@ -3,21 +3,33 @@
 import 'dart:ui' as ui; // 调用window拿到route判断跳转哪个界面
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'pagea.dart';
 import 'saveList.dart';
+import 'goods_detail.dart';
+import 'route.dart';
+void main() => runApp(const MyApp());
 
-void main() => runApp(_widgetForRoute(ui.window.defaultRouteName));
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
-// 根据iOS端传来的route跳转不同界面
-Widget _widgetForRoute(String route) {
-  switch (route) {
-    case 'myApp':
-      return new MyApp();
-    case 'home':
-      return new MyApp();
-    default:
-      return Center(
-        child: Text('Unknown route: $route', textDirection: TextDirection.ltr),
-      );
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '美味不用等',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or press Run > Flutter Hot Reload in a Flutter IDE). Notice that the
+        // counter didn't reset back to zero; the application is not restarted.
+        primarySwatch: Colors.red,
+      ),
+      //todo 单元测试入口，发布改成'/'
+      initialRoute: "/saveList",
+      routes: MWRouter.routes,
+    );
   }
 }
--- lib/route.dart
+++ lib/route.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'goods_detail.dart';
-
+import 'saveList.dart';
 class MWRouter{
   static final Map<String,WidgetBuilder> routes = {
     //商品详情
     "/goods/detail": (context) => GoodsDetail(),
+    "/saveList": (context) => MyApp(),
   };
 }
